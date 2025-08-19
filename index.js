0ea6393c640c7cb933592f13a45a080260c4fec0
Juan Martín Grado <114537878+Juanmgrado@users.noreply.github.com>
2025-08-19 19:12:03 +0200

    erase consoles logs

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-console.log("Script cargado")
 
 
-const wordsList = ["papa", "perro", "auricular"]
+const wordsList = ["papa", "perro", "auricular", "alpargata", "ab"]
 const wordListLength = wordsList.length;
 const buttonsLetters = document.querySelectorAll(".button-letter")
 let randomNumber = Math.floor(Math.random() * wordListLength)
@@ -10,44 +9,45 @@ const randomWord = wordsList[randomNumber].split("")
 let hiddenWord = randomWord.map(() => "_");
 let life = 5
 wordInput.value = hiddenWord
-console.log(randomWord)
-console.log(hiddenWord)
-
 
 const unHiddenWord = (letter, word, hiddenWord) =>{
-
-    const indexsLetter = word.reduce((acc, lett, index) =>{
-        if(lett === letter ){
-            acc.push(index)
-        }
-        return acc
-    }, [])
-
-    console.log(indexsLetter);
     
+    const indexsLetter = word.reduce((matches, character, index) =>{
+        if(character === letter ){
+            matches.push(index)
+        }
+        return matches
+    }, [])
+        
     if (life === 0){
-        alert("SOS UNA VERGA MAN PERDISTE")
+        alert("MAN PERDISTE")
         return randomWord
     }
     
-    let partialWord = hiddenWord.map((lett, index) =>{
+    let partialWord = hiddenWord.map((character, index) =>{
         if(indexsLetter.includes(index)){
-            lett = letter
+            character = letter
             return letter
         }else{
-            return lett
+            return character
         }
         
     })
+
     if(indexsLetter.length === 0){
         life -= 1
         alert("ACABAS DE PERDER 1 VIDA")
         return partialWord
     }
     
-    console.log(partialWord);
     return partialWord
 
+}
+const checkWin = (word, partialWord) =>{
+     if(word.length === partialWord.length && word.every((character, index) => character === partialWord[index])){
+        alert ("FELICITACIONES HAS GANADO!!!!")
+        return 
+    }
 }
 
 const knowWord = () => {
@@ -56,9 +56,9 @@ const knowWord = () => {
         letter.addEventListener(("click"), event => {
             const selectedLetter = event.target.innerText
             const letterLower = selectedLetter.toLowerCase()
-            console.log(selectedLetter);     
             hiddenWord = unHiddenWord(letterLower, randomWord, hiddenWord);
-            wordInput.value = hiddenWord 
+            wordInput.value = hiddenWord
+            checkWin(randomWord, hiddenWord) 
                 
             
         })
