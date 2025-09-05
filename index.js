1c4228ab17566fb312d24cc212a0039088d049e7
Juan Martín Grado <114537878+Juanmgrado@users.noreply.github.com>
2025-09-05 15:22:52 +0200

    fix name variable where it was wrong

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const startCountdown = (duration) => {
 
             matchTime.value = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 
-            if (life < -1) { 
+            if (numberOfChances < -1) { 
                 clearInterval(timer); 
             }
         },
